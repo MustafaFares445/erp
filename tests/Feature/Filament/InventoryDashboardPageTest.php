6614ab91853d1fbea2004e5035510f10dd046371
Mustafa Fares <mustaphafares445@gmail.com>
2026-09-03 15:21:39 +0300

    test(filament): pin quarantine ageing dashboard placement

--- tests/Feature/Filament/InventoryDashboardPageTest.php
+++ tests/Feature/Filament/InventoryDashboardPageTest.php
@@ -9,6 +9,7 @@ use App\Filament\Widgets\InventoryLowStock;
 use App\Filament\Widgets\InventoryMovementsTrend;
 use App\Filament\Widgets\InventoryOperationsPipeline;
 use App\Filament\Widgets\InventoryPendingDocuments;
+use App\Filament\Widgets\InventoryQuarantineAgeing;
 use App\Filament\Widgets\InventoryRecentMovements;
 use App\Filament\Widgets\ReconciliationStatus;
 use App\Filament\Widgets\InventoryStockStatistics;
@@ -29,6 +30,7 @@ it('registers the redesigned widget set in the intended reading order', function
 
     expect($widgets)->toBe([
         InventoryKeyMetrics::class,
+        InventoryQuarantineAgeing::class,
         ReconciliationStatus::class,
         InventoryOperationsPipeline::class,
         InventoryPendingDocuments::class,
