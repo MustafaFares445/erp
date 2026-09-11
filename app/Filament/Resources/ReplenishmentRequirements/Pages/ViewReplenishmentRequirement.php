046769ec3c68137058dd560325113ff7e6fb9f88
Mustafa Fares <mustaphafares445@gmail.com>
2026-09-11 12:45:38 +0300

    feat(replenishment): add requirement detail page

---
+++ app/Filament/Resources/ReplenishmentRequirements/Pages/ViewReplenishmentRequirement.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Resources\ReplenishmentRequirements\Pages;
+
+use App\Filament\Resources\ReplenishmentRequirements\ReplenishmentRequirementResource;
+use Filament\Resources\Pages\ViewRecord;
+
+final class ViewReplenishmentRequirement extends ViewRecord
+{
+    protected static string $resource = ReplenishmentRequirementResource::class;
+}
