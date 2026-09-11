5c13009144f20605d1cc489dbe2e69fcc15d20a8
Mustafa Fares <mustaphafares445@gmail.com>
2026-09-11 12:44:49 +0300

    feat(replenishment): authorize requirement work queue

---
+++ app/Policies/ReplenishmentRequirementPolicy.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Policies;
+
+use App\Enums\InventoryPermission;
+use App\Models\User;
+use App\Policies\Concerns\ChecksInventoryPermissions;
+
+final class ReplenishmentRequirementPolicy
+{
+    use ChecksInventoryPermissions;
+
+    public function viewAny(User $user): bool
+    {
+        return $this->authorizeInventoryAbility($user, 'viewAny');
+    }
+
+    public function view(User $user): bool
+    {
+        return $this->authorizeInventoryAbility($user, 'view');
+    }
+
+    public function create(): bool
+    {
+        return false;
+    }
+
+    public function update(): bool
+    {
+        return false;
+    }
+
+    public function delete(): bool
+    {
+        return false;
+    }
+
+    /** @return array<string, string> */
+    protected function inventoryPermissionMap(): array
+    {
+        return [
+            'viewAny' => InventoryPermission::StockView->value,
+            'view' => InventoryPermission::StockView->value,
+        ];
+    }
+}
