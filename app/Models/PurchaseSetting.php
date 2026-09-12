d730f93246275e93c13f36eabcb633b4163f0147
Mustafa Fares <mustaphafares445@gmail.com>
2026-09-12 05:33:00 +0300

    feat(accounting): configure GRNI account

--- app/Models/PurchaseSetting.php
+++ app/Models/PurchaseSetting.php
@@ -8,6 +8,7 @@ use Database\Factories\PurchaseSettingFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
  * Singleton settings row for purchasing, following the {@see InventorySetting}
@@ -16,7 +17,7 @@ use Illuminate\Database\Eloquent\Model;
  * @property string $approval_threshold_amount
  * @property string $approval_threshold_currency
  */
-#[Fillable(['approval_threshold_amount', 'approval_threshold_currency'])]
+#[Fillable(['approval_threshold_amount', 'approval_threshold_currency', 'grni_account_id'])]
 final class PurchaseSetting extends Model
 {
     /** @use HasFactory<PurchaseSettingFactory> */
@@ -34,5 +35,11 @@ final class PurchaseSetting extends Model
             'approval_threshold_amount' => 0,
             'approval_threshold_currency' => 'AED',
         ]);
+    }
+
+    /** @return BelongsTo<ChartAccount, $this> */
+    public function grniAccount(): BelongsTo
+    {
+        return $this->belongsTo(ChartAccount::class, 'grni_account_id');
     }
 }
