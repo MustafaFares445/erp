332293dfdbbb0d7c089b25c20f283ff01f6ab698
Mustafa Fares <mustaphafares445@gmail.com>
2026-09-13 22:36:10 +0300

    style: normalize purchase inbound line model

--- app/Models/PurchaseInboundLine.php
+++ app/Models/PurchaseInboundLine.php
@@ -7,6 +7,7 @@ namespace App\Models;
 use App\Services\Purchasing\PurchaseInboundService;
 use Database\Factories\PurchaseInboundLineFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -26,7 +27,7 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
  * @property int $purchase_order_line_id
  * @property PurchaseInbound $purchaseInbound
  * @property PurchaseOrderLine $purchaseOrderLine
- * @property \Illuminate\Database\Eloquent\Collection<int, PurchaseInboundAllocation> $allocations
+ * @property Collection<int, PurchaseInboundAllocation> $allocations
  * @property PurchaseInboundAllocation|null $allocation
  */
 #[Fillable([
